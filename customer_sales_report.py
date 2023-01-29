d7ebbc1bffc28cf14ba16cea3b47b4831df592c9
Kenneth Schoel <Kenneth_schoel1@baylor.edu>
2023-01-28 20:09:59 -0600

    Back to Start

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -1,15 +1,11 @@
 import pandas as pd
+data = pd.read_csv("sales.csv")
 myfile = open("salesreport.csv", "w+")
-data = pd.read_csv('sales.csv')
-x = (data.filter(regex='CustomerID'))
-q = (data.filter(regex='SubTotal'))
-w = (data.filter(regex='TaxAmt'))
-e = (data.filter(regex='Freight'))
-#data.sum(SubTotal,TaxAmt,Freight)
-#grouped_data = data.groupby("CustomerID").sum({"SubTotal"+"TaxAmt"+"Freight"}
-
-data = data.rename(columns={'SubTotal': "Total"})
-grouped_data = data.groupby("CustomerID").sum("Total")
-grouped_data = grouped_data.to_string()
-myfile.write(grouped_data)
-y = (data.filter(regex='SubTotal|TaxAmt|Freight'))
+data['Total'] = data['TaxAmt']+ data['SubTotal']  + data['Freight']
+id = data.loc[:, ['CustomerID', "Total"]]
+id = id.groupby("CustomerID").sum()
+x =data.head(10)
+x = x.to_string()
+myfile.write(x)
+id.to_csv("salesreport.csv", index=True)
+    
