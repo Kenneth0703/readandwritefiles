87aec71111a6ca48cf5e2cf91d46d92d086cb652
Kenneth Schoel <Kenneth_schoel1@baylor.edu>
2023-01-30 18:19:25 -0600

    part 2

--- customer_country.py
+++ customer_country.py
@@ -1,10 +1,12 @@
 # i need to program that reads the file customers.csv and produces a new file 
 # containing the customer name and country they are from
-import pandas as pd
-myfile = open("customers and countries.csv", "w+")
-data = pd.read_csv('customers.csv')
-x = (data.filter(regex='Name|Country'))
-x= x.to_string()
-myfile.write(x)
+import csv
+infile = open("customers.csv","r")
+newfile = open("customer_country.csv", "w+")
+read = csv.reader(infile)
+writer = csv.writer(newfile)
 
+for row in read:
+    rows = [row[1],row[1],row[4]]
+    writer.writerow(rows)
 
